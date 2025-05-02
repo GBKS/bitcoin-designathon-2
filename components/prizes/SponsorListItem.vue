98c247419a199c4b60a028ad0c13c426ce2d137e
Christoph Ono <chri@sto.ph>
2025-05-02 16:18:35 +0200

    Add Blockstream as a sponsor (#12)
    Oh yeah! Major thanks to those block streamers.

--- components/prizes/SponsorListItem.vue
+++ components/prizes/SponsorListItem.vue
@@ -40,29 +40,12 @@ const sponsorSourceSet = computed(() => {
       <h3>{{ info.title }}</h3>
       <p class="-description" v-html="info.description" />
       <p class="-prize" v-html="info.prize" />
-      <div class="sponsor" v-if="info.sponsor">
-        <img
-          :src="`/images/sponsors/${info.sponsor.image}.png`"
-          :srcset="sponsorSourceSet"
-          width="50"
-          height="50"
-          :alt="info.sponsor.name"
-          loading="lazy"
-        >
-        <div class="right">
-          <p>Sponsor</p>
-          <h5>
-            <a
-              v-if="info.sponsor.url"
-              :href="info.sponsor.url"
-              target="_blank"
-              rel="nofollow noindex"
-            >{{ info.sponsor.name }}</a>
-            <template v-else>
-              {{ info.sponsor.name }}
-            </template>
-          </h5>
-        </div>
+      <div class="sponsors" v-if="info.sponsors">
+        <PrizesSponsorDetails
+          v-for="sponsor in info.sponsors"
+          :key="index"
+          :info="sponsor"
+        />
       </div>
     </div>
   </div>
@@ -118,36 +101,11 @@ const sponsorSourceSet = computed(() => {
       }
     }
 
-    .sponsor {
-      margin-top: 15px;
+    .sponsors {
+      margin-top: 25px;
       display: flex;
-      align-items: center;
-      gap: 10px;
-
-      img {
-        border-radius: 15px;
-      }
-
-      .right {
-        p {
-          color: rgba(black, 0.75);
-          @include mixins.r('font-size', 15, 17);
-        }
-
-        h5 {
-          color: rgba(black, 1);
-          @include mixins.r('font-size', 15, 19);
-
-          a {
-            text-decoration: none;
-            color: rgba(black, 1);
-
-            &:hover {
-              text-decoration: underline;
-            }
-          }
-        }
-      }
+      flex-direction: column;
+      gap: 15px;
     }
   }
 
